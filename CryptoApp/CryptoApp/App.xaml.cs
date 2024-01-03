73f1a3e5d63c1f717161c94c3bef23fdd73b760a
Dominik Litka <dominiklitka6@gmail.com>
2024-01-03 12:06:43 +0100

    Init project

---
+++ CryptoApp/CryptoApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CryptoApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ CryptoApp/CryptoApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CryptoApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
